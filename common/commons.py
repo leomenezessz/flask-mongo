b344fbd464b1cc386403263ec88b2b52731e091c
Leonardo <leonardosmenezes.ssz@gmail.com>
2019-12-02 03:52:03 -0300

    add request validator, response creator, use configs environments, change resources paths

--- common/commons.py
+++ common/commons.py
@@ -1,15 +1,25 @@
 from uuid import uuid4
+from bson.json_util import dumps
+from flask import Response, request
 
 
 def generate_str_uuid4() -> str:
     return str(uuid4())
 
 
-def dict_have_key(dict: dict, key):
-    if key in dict:
-        return True
-    return False
+def make_resp(data, code, mimetype="application/json"):
+    return Response(dumps(data), code, mimetype=mimetype)
 
 
-def msg(msg_param):
-    return {"message": msg_param}
+def msg_payload(msg):
+    return {"message": msg}
+
+
+def is_valid_fields(req: request, fields):
+    if req.json is None:
+        return False
+
+    for field in fields:
+        if field not in req.json:
+            return False
+    return True
